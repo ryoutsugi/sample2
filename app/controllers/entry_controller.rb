e8715fd004e4a6441eb709332d2bfa36de3382e4
utsugi <urjn12345@gmail.com>
2017-02-11 19:01:17 +0900

    add新規作成画面完成

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -11,7 +11,23 @@ class EntryController < ApplicationController
     @entry = Entry.new
   end
 
+  def edit
+    @entry =  Entry.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   def create
-    
+    entry = params.require(:entry).permit(:name, :entrybody)
+    entry = Entry.new(entry)
+    if entry.save
+      redirect_to entry
+    else
+      render :new
+    end
   end
-  end
+end
